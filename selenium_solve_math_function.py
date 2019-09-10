ee3182108ddbe76898cff55cc46de3b7c94c54bf
AM <anvar.minulin@gmail.com>
2019-09-10 23:09:22 +0300

    button by xpath, get attribute value, solve math function

---
+++ selenium_solve_math_function.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+import math 
+
+link = "http://suninjuly.github.io/math.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    xElement = browser.find_element_by_id("input_value")
+    x = xElement.text
+    y = str(math.log(abs(12*math.sin(int(x)))))
+    answerField = browser.find_element_by_id("answer")
+    answerField.send_keys(y)
+    isRobot = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    isRobot.click()
+    rule = browser.find_element_by_id("robotsRule")
+    rule.click()
+    submit = browser.find_element_by_class_name("btn")
+    submit.click()
+
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
