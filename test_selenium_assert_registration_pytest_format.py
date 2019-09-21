8365a4481e98691a482cac206cd513ca90934f5b
AM <anvar.minulin@gmail.com>
2019-09-21 14:46:38 +0300

    Test file was changesd for using with pytest. Pytest.raises

---
+++ test_selenium_assert_registration_pytest_format.py
@@ -0,0 +1,14 @@
+from selenium_assert_registration import registration_form_check
+
+class TestRegistration(unittest.TestCase):
+    message = "Congratulations! You have successfully registered!"
+    failed = "Text should be: Congratulations! You have successfully registered!"
+    
+    def test_first_link(self):
+        assert registration_form_check("http://suninjuly.github.io/registration1.html") == self.message, self.failed
+        
+    def test_second_link(self):
+        assert registration_form_check("http://suninjuly.github.io/registration2.html") == self.message, self.failed
+        
+if __name__ == "__main__":
+    unittest.main()
