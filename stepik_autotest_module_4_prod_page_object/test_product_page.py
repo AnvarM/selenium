9920058b691d841cd4c1ff3419da97b1557e6620
AM <anvar.minulin@gmail.com>
2019-11-03 22:35:17 +0300

    Mark tests for review

--- stepik_autotest_module_4_prod_page_object/test_product_page.py
+++ stepik_autotest_module_4_prod_page_object/test_product_page.py
@@ -4,49 +4,27 @@ from .pages.login_page import LoginPage
 import pytest
 import time
 
-# def test_guest_can_add_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_cart()
-#     page.is_added_to_basket_item_correct()
-#     page.is_price_correct()
-#
-# @pytest.mark.xfail
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_cart()
-#     page.should_not_be_success_message()
-#
-# def test_guest_cant_see_success_message(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_not_be_success_message()
-#
-# @pytest.mark.xfail
-# def test_message_disappeared_after_adding_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_cart()
-#     page.success_message_is_disapeared()
+@pytest.mark.need_review
+def test_user_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.is_added_to_basket_item_correct()
+    page.is_price_correct()
 
-# def test_guest_should_see_login_link_on_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_be_login_link()
-#
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.is_added_to_basket_item_correct()
+    page.is_price_correct()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -55,6 +33,43 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.no_goods_in_basket()
     basket_page.check_basket_is_empty()
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.success_message_is_disapeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 @pytest.mark.product_page
 class TestUserAddToBasketFromProductPage():
@@ -74,11 +89,3 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
-    def test_user_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-        #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        page = ProductPage(browser, link)
-        page.open()
-        page.add_to_cart()
-        page.is_added_to_basket_item_correct()
-        page.is_price_correct()
